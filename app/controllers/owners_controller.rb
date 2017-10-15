7d86bdc0e0ac8b0c92df42fdfb343161f51c1608
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-15 23:09:39 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,7 +11,12 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
-
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect to "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
