385af68610e69b85393f7a91bfbbf2dd50cb757a
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-15 23:40:07 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "owners", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string  "name"
+    t.integer "owner_id"
   end
 
 end
